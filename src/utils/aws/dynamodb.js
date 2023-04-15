afb7e0c598baa7d592f5bf1dd31c0b361febee0d
Razvan Tamsa <razvantamsa295@gmail.com>
2023-04-15 13:50:15 +0300

    add dynamodb get-batch and delete-one

--- src/utils/aws/dynamodb.js
+++ src/utils/aws/dynamodb.js
@@ -18,9 +18,31 @@ const getItem = async (TableName, Key) => {
     };
     const result = await dynamoDB.get(params).promise();
     return result.Item;
+};
+
+const getItemByPk = async (TableName, pkName, pkValue) => {
+  const params = {
+    TableName,
+    KeyConditionExpression: `${pkName} = :${pkName}`,
+    ExpressionAttributeValues: {
+      [`:${pkName}`]: pkValue
+    }
   };
+  const result = await dynamoDB.query(params).promise();
+  return result.Items;
+};
+
+const deleteItem = async (TableName, Key) => {
+  const params = {
+    TableName,
+    Key
+  };
+  await dynamoDB.delete(params).promise();
+};
 
 module.exports = {
-    postItem,
-    getItem,
+  getItem,
+  getItemByPk,
+  postItem,
+  deleteItem,
 }
