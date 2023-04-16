d62c8d6a4065b78ec0204ae82302d1b839897786
Razvan Tamsa <razvantamsa295@gmail.com>
2023-04-16 21:05:30 +0300

    implement dotenv logic for aws

--- src/utils/aws/dynamodb.js
+++ src/utils/aws/dynamodb.js
@@ -1,6 +1,11 @@
+require('dotenv').config({ path: 'src/utils/aws/credentials.env' });
 const AWS = require('aws-sdk');
 
-AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: 'sneaker-app' });
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 const postItem = async (TableName, Item) => {
