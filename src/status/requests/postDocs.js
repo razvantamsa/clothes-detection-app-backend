b5154c1f243e0cc55fd0c41f85fa4989cee231b2
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-01 20:51:02 +0300

    fix issue with secretsmanager

--- src/status/requests/postDocs.js
+++ src/status/requests/postDocs.js
@@ -6,22 +6,7 @@ const router = express.Router();
 const { S3_DOCS_BUCKET } = process.env;
 
 router.post('/', authorizerMiddleware, async (req, res) => {
-    // try {
-    //     const data = await getItem(S3_DOCS_BUCKET, 'openapi.html');
-    //     const htmlContent = data.Body.toString('utf-8');
-    //     return res
-    //         .status(200)
-    //         .header('Content-Type', 'text/html')
-    //         .send(htmlContent);
-
-    // } catch (err) {
-    //     console.log(err);
-    //     res.status(400).send(err);
-    // }
     try {
-        // const files = Object.entries(req.files)
-        //     .map(([key, value]) => Object.assign(value, {name: key}));
-    
         const file = req.files.docs;
         await postItem(
             S3_DOCS_BUCKET,
