f647dd2490298b87fd5edcf18f6b107c6923d0ef
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-02 17:18:38 +0300

    scan result endpoint completed

--- src/scan/requests/getScanResult.js
+++ src/scan/requests/getScanResult.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { getItem: getDynamoDB } = require('../../utils/aws/dynamodb');
+const { getSignedUrl } = require('../../utils/aws/s3');
 
 // workflow:
 /**
@@ -7,9 +9,17 @@ const router = express.Router();
  * return status: /processed + relevant data / to be processed
  */
 
+const { DYNAMODB_SCAN_TABLE, S3_SCAN_BUCKET } = process.env;
+
 router.get('/:dataId', async (req, res) => {
     try {
-        res.status(200).send('ok');
+        const { dataId } = req.params;
+        const userName = req.headers.user;
+
+        const scanResult = await getDynamoDB(DYNAMODB_SCAN_TABLE, { dataId, userName });
+        const image = await getSignedUrl(S3_SCAN_BUCKET, dataId);
+
+        res.status(200).send({ scanResult, image });
     } catch (err) {
         console.log(err);
         res.status(400).send(err);
