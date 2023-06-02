331fb5b54301e4e66a7a814b7e3e3f6e3f47cf0a
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-02 14:02:48 +0300

    boilerplate extract features & start scan completed

--- src/scan/requests/startScan.js
+++ src/scan/requests/startScan.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const { generateUniqueHashCode } = require('../../utils/authorizer/hashCode');
+const { postItem: postS3 } = require('../../utils/aws/s3');
+const { postItem: postDynamoDB } = require('../../utils/aws/dynamodb');
+const { invokeAsyncFunction } = require('../../utils/aws/lambda');
 const router = express.Router();
 
 // workflow:
@@ -7,12 +11,41 @@ const router = express.Router();
  * trigger processing lambda for s3 image
  */
 
+const { DYNAMODB_SCAN_TABLE, S3_SCAN_BUCKET } = process.env;
+
 router.post('/', async (req, res) => {
     try {
-        res.status(200).send('ok');
+
+        const user = req.headers.user;
+        const file = req.files.image;
+
+        const dataId = generateUniqueHashCode();
+
+        await postS3(
+            S3_SCAN_BUCKET, 
+            `${dataId}`, 
+            file.data,
+            file.data.length,
+            "image/jpeg"
+        );
+
+        await postDynamoDB(DYNAMODB_SCAN_TABLE, { 
+            dataId, 
+            user, 
+            status: 'unprocessed',
+            fileName: file.name
+        });
+
+        await invokeAsyncFunction(
+            'clothes-detection-scan-dev-extractFeatures',
+            { dataId },
+        );
+
+        return res.status(200).send({ dataId });
+
     } catch (err) {
         console.log(err);
-        res.status(400).send(err);
+        return res.status(400).send(err.message);
     }
 });
   
