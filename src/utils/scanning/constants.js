de916472428c462489b616256654e0ab63dce11a
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-03 16:12:44 +0300

    add labels list

---
+++ src/utils/scanning/constants.js
@@ -0,0 +1,65 @@
+const shirtLabels = [
+    'Shirt',
+    'T-Shirt',
+    'Blouse',
+    'Top',
+    'Polo Shirt',
+    'Button-Down Shirt',
+    'Sweatshirt',
+    'Hoodie',
+    'Jacket',
+    'Coat',
+    'Cardigan',
+    'Sweater',
+    'Vest',
+    'Tank Top',
+    'Camisole',
+    'Pullover',
+    'Tunic',
+    'Peplum Top',
+    'Bodysuit',
+    'Kimono',
+    'Shawl',
+    'Wrap'
+];
+
+const trousersLabels = [
+    'Pants',
+    'Jeans',
+    'Trousers',
+    'Leggings',
+    'Shorts',
+    'Skirt',
+    'Dress',
+    'Jumpsuit',
+    'Capri Pants',
+    'Cargo Pants',
+    'Chinos',
+    'Culottes',
+    'Overalls',
+    'Sweatpants',
+    'Swim Trunks',
+    'Harem Pants',
+    'Pencil Skirt',
+    'Maxi Skirt',
+    'Midi Skirt',
+    'Mini Skirt',
+    'Flare Skirt'
+];
+
+const shoeLabels = [
+    'Shoe',
+    'Footwear',
+    'Sneaker',
+    'Boot',
+    'Running Shoe',
+    'Sports Shoe',
+    'Athletic Shoe',
+];
+
+module.exports = {
+    shirtLabels,
+    trousersLabels,
+    shoeLabels
+}
+    
