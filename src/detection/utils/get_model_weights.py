24d4033553d40cafc687c074d32f5e2e4fff7530
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-12 20:03:13 +0300

    minimize code

--- src/detection/utils/get_model_weights.py
+++
@@ -1,4 +0,0 @@
-def handler(s3, bucket_name, object_key):
-    print(bucket_name, object_key)
-    s3.download_file(bucket_name, object_key, 'model_weights.h5')
-    print('File downloaded successfully.')
