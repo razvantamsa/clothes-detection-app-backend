f87a341bbcca5ee0b96fda1c42e2fd7e484cf81c
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-12 19:05:02 +0300

    generalize get list of classes

--- src/detection/utils/get_list_of_classes.py
+++ src/detection/utils/get_list_of_classes.py
@@ -1,6 +1,6 @@
-def handler(s3, bucket_name):
+def handler(s3, bucket_name, prefix = ""):
 
-    response = s3.list_objects_v2(Bucket=bucket_name, Delimiter="/")
+    response = s3.list_objects_v2(Bucket=bucket_name, Delimiter="/", Prefix=prefix)
 
     # Extract root folders from the response
     root_folders = [prefix["Prefix"] for prefix in response.get("CommonPrefixes", [])]
