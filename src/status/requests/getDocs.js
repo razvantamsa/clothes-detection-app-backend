9f4083bb963c73e7e11fad1b5e02ea207af7e240
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-18 20:36:42 +0300

    update get-docs lamdba

--- src/status/requests/getDocs.js
+++ src/status/requests/getDocs.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const { getItem } = require('../../utils/aws/s3');
 const router = express.Router();
 
 const { S3_ASSETS_BUCKET } = process.env;
 
+const DOCS_FILE_PATH = '/tmp/openapi.html', DOCS_FILE_NAME = 'openapi.html';
+
+async function downloadDocs() {
+    const data = await getItem(S3_ASSETS_BUCKET, DOCS_FILE_NAME);
+    const fileStream = fs.createWriteStream(DOCS_FILE_PATH);
+    fileStream.write(data.Body);
+    fileStream.end();
+}
+
 router.get('/', async (req, res) => {
     try {
-        const data = await getItem(S3_ASSETS_BUCKET, 'openapi.html');
-        const htmlContent = data.Body.toString('utf-8');
+        const fileExists = fs.existsSync(DOCS_FILE_PATH);
+        if(!fileExists) {
+            await downloadDocs();
+        }
+        const htmlContent = fs.readFileSync(DOCS_FILE_PATH);
         return res
             .status(200)
             .header('Content-Type', 'text/html')
