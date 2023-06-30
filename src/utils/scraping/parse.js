314e803cd93ab01da3b3e07efb85c913999a122e
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-30 19:46:10 +0300

    make efficient use ofenvvars

--- src/utils/scraping/parse.js
+++ src/utils/scraping/parse.js
@@ -5,26 +5,18 @@ function getStackName(functionName) {
 }
 
 function getEnvironmentVariables(logGroupName) {
+    const envVars = { APP_LOG_GROUP_NAME: logGroupName };
     const currentDate = new Date();
 
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, '0');
     const day = String(currentDate.getDate()).padStart(2, '0');
     const time = currentDate.toISOString().split('T')[1].replace(/\..+/, '');
-    const logStreamName = `${year}/${month}/${day}/[$LATEST]${time}`;
+    envVars.APP_LOG_STREAM_NAME = `${year}/${month}/${day}/[$LATEST]${time}`;
 
-    return {
-        DYNAMODB_SHIRTS_TABLE: process.env.DYNAMODB_SHIRTS_TABLE,
-        S3_SHIRTS_BUCKET: process.env.S3_SHIRTS_BUCKET,
-        DYNAMODB_TROUSERS_TABLE: process.env.DYNAMODB_TROUSERS_TABLE,
-        S3_TROUSERS_BUCKET: process.env.S3_TROUSERS_BUCKET,
-        DYNAMODB_SHOES_TABLE: process.env.DYNAMODB_SHOES_TABLE,
-        S3_SHOES_BUCKET: process.env.S3_SHOES_BUCKET,
-        MAX_PRODUCT_LIMIT: process.env.MAX_PRODUCT_LIMIT,
-        PRODUCT_RATE_LIMIT:process.env.PRODUCT_RATE_LIMIT,
-        LOG_GROUP_NAME: logGroupName,
-        LOG_STREAM_NAME: logStreamName
-    }
+    return Object.entries(process.env)
+        .filter(([key, value]) => key.startsWith('APP_'))
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), envVars);
 }
 
 function getBaseImageUri(accountId) {
