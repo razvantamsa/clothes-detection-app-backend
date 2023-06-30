5531cb530266dbe6a6e6c9c90bbb848056aa008c
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-30 19:19:52 +0300

    replace all occurences in project

--- src/scan/lambdas/extractFeatures.js
+++ src/scan/lambdas/extractFeatures.js
@@ -6,13 +6,13 @@ const { getItem: getS3, postItem: postS3 } = require("../../utils/aws/s3");
 const { cropImage } = require("../../utils/scanning/cropImage");
 const { nonOverlapingLabels } = require("../../utils/scanning/nonOverlapingLabels");
 
-const { DYNAMODB_SCAN_TABLE, S3_SCAN_BUCKET } = process.env;
+const { APP_DYNAMODB_SCAN_TABLE, APP_S3_SCAN_BUCKET } = process.env;
 
 exports.handler = async (event, context) => {
     console.log('Event payload: ', event);
     const { dataId, userName } = event;
 
-    const labels = await detectImageLabels(S3_SCAN_BUCKET, dataId);
+    const labels = await detectImageLabels(APP_S3_SCAN_BUCKET, dataId);
     console.log(labels);
     const filteredLabels =  [
         ...nonOverlapingLabels(labels, 'shirts'),
@@ -21,7 +21,7 @@ exports.handler = async (event, context) => {
     ];
     console.log(filteredLabels);
 
-    const image = await getS3(S3_SCAN_BUCKET, dataId);
+    const image = await getS3(APP_S3_SCAN_BUCKET, dataId);
     const croppedImages = await Promise.all(filteredLabels.map(async (label) => {
         const croppedImage = await cropImage(image.Body, label);
         return {...label, data: croppedImage};
@@ -32,14 +32,14 @@ exports.handler = async (event, context) => {
         const croppedDataId = calculateImageHash(croppedImage.data);
 
         await postS3(
-            S3_SCAN_BUCKET,
+            APP_S3_SCAN_BUCKET,
             croppedDataId,
             croppedImage.data,
             croppedImage.data.length,
             "image/jpeg"
         );
 
-        await postDynamoDB(DYNAMODB_SCAN_TABLE, {
+        await postDynamoDB(APP_DYNAMODB_SCAN_TABLE, {
             dataId: croppedDataId,
             userName: dataId, 
             status: 'processing',
@@ -58,7 +58,7 @@ exports.handler = async (event, context) => {
     }));
 
     // update main dataId entry in dynamodb
-    await updateDynamoDB(DYNAMODB_SCAN_TABLE,
+    await updateDynamoDB(APP_DYNAMODB_SCAN_TABLE,
         { dataId, userName },
         { status: 'processed' },
     );
