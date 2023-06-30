5ce3a725a7fddebd22a8a566b5d0bd9fa650b719
Razvan Tamsa <razvantamsa295@gmail.com>
2023-06-30 19:59:42 +0300

    rename file

--- src/utils/scraping/splitArray.js
+++ src/utils/scraping/splitArray.js
@@ -2,3 +2,5 @@ function splitArray(array, m) {
     const chunkSize = Math.ceil(array.length / m);
     return Array.from({ length: m }, (_, i) => array.slice(i * chunkSize, (i + 1) * chunkSize));
 }
+
+module.exports = { splitArray }
