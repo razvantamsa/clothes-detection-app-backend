21c1999fd8062fc556b8726b91700d3e37df77be
Razvan Tamsa <razvantamsa295@gmail.com>
2023-07-14 15:33:51 +0300

    cloudwatch logs aws utils

---
+++ src/utils/aws/cloudwatch.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: 'src/utils/aws/credentials.env' });
+const AWS = require('aws-sdk');
+
+if (!process.env.AWS_EXECUTION_ENV) {
+  AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION_NAME
+  });
+}
+
+const cloudWatchLogs = new AWS.CloudWatchLogs()
+
+module.exports = { cloudWatchLogs }
