a6a547b0fc7f31e5e23a5922dc26ee204dc1ecc1
Razvan Tamsa <razvantamsa295@gmail.com>
2023-07-15 17:49:46 +0300

    bundle with webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    mode: 'development',
+    entry: slsw.lib.entries,
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        libraryTarget: 'commonjs2',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
+};
