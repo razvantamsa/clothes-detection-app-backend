32eb18ecf9e507d8410f00f720b87b2c5b3d4794
Razvan Tamsa <razvantamsa295@gmail.com>
2023-07-17 11:59:16 +0300

    log only real messages

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -1,18 +1,22 @@
 const { logToCloudWatch } = require("../aws/cloudwatch");
 
 const logging = (logGroupName, logStreamName, level, message) => {
-    const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME || 'unknown';
-    const logEvent = [{
-      message: `${level}: ${functionName} - ${message}`,
-      timestamp: Date.now(),
-    }];
+  if(!message) {
+    return;
+  }
   
-    logToCloudWatch(logGroupName, logStreamName, logEvent);
+  const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME || 'unknown';
+  const logEvent = {
+    message: `${level}: ${functionName} - ${message}`,
+    timestamp: Date.now(),
+  };
+
+  logToCloudWatch(logGroupName, logStreamName, logEvent);
 };
 
 const logger = (logGroupName = `microservice/${process.env.SERVICE}`, logStreamName = 'index') => ({
-    info: (message) => logging(logGroupName, logStreamName, 'INFO', message),
-    error: (message) => logging(logGroupName, logStreamName, 'ERROR', message),
+  info: (message) => logging(logGroupName, logStreamName, 'INFO', message),
+  error: (message) => logging(logGroupName, logStreamName, 'ERROR', message),
 })
 
 module.exports = logger;
