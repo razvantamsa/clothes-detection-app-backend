94bd5a183894ca0b69e84b2fe72bb74827539a73
Razvan Tamsa <razvantamsa295@gmail.com>
2023-07-17 16:21:58 +0300

    logger to display complex messages

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -1,9 +1,12 @@
 const { logToCloudWatch } = require("../aws/cloudwatch");
 
-const logging = (logGroupName, logStreamName, level, message) => {
-  if(!message) {
+const logging = (logGroupName, logStreamName, level, ...args) => {
+  if(!args.length) {
     return;
   }
+
+  const messages = args.map(arg => typeof arg === 'object' ? JSON.stringify(arg) : arg);
+  const message = messages.join(' ');
 
   const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME || 'unknown';
   const logEvent = [{
@@ -15,8 +18,8 @@ const logging = (logGroupName, logStreamName, level, message) => {
 };
 
 const logger = (logGroupName = `microservice/${process.env.SERVICE}`, logStreamName = 'index') => ({
-  info: (message) => logging(logGroupName, logStreamName, 'INFO', message),
-  error: (message) => logging(logGroupName, logStreamName, 'ERROR', message),
+  info: (...args) => logging(logGroupName, logStreamName, 'INFO', ...args),
+  error: (...args) => logging(logGroupName, logStreamName, 'ERROR', ...args),
 })
 
 module.exports = logger;
