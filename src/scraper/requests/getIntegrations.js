86da49a4b77097a82443df8262375c3a3be57eb4
Razvan Tamsa <razvantamsa295@gmail.com>
2023-08-01 19:26:19 +0300

    query by gsi - brand

--- src/scraper/requests/getIntegrations.js
+++ src/scraper/requests/getIntegrations.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { scanTable, getItemByPk } = require('../../utils/aws/dynamodb');
+const { scanTable, getItemByPk, queryTableByGSI } = require('../../utils/aws/dynamodb');
 const logger = require('../../utils/logger')();
 const router = express.Router();
 
 router.get('/integration', async (req, res) => {
     try {
         let items;
-        if(req.headers.website) {
-            items = await getItemByPk('scraper-integration-table', 'website', req.headers.website); 
+        const { website, brand } = req.headers;
+        if(website) {
+            items = await getItemByPk('scraper-integration-table', 'website', website); 
+        } else if (brand) {
+            items = await queryTableByGSI('scraper-integration-table', 'brand-gsi', { brand });
         } else {
             items = await scanTable('scraper-integration-table')
         }
