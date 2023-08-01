2522b4bb867700a4de97a25158b916f3042b0e6e
Razvan Tamsa <razvantamsa295@gmail.com>
2023-08-01 18:49:55 +0300

    implement updated dynamdob scantable

--- src/scraper/requests/getScrapingIntegrations.js
+++ src/scraper/requests/getScrapingIntegrations.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { listAllTableEntries } = require('../../utils/aws/dynamodb');
+const { scanTable } = require('../../utils/aws/dynamodb');
 const logger = require('../../utils/logger')();
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const items = await listAllTableEntries('scraper-integration-table', 'website, brand')
+        const items = await scanTable('scraper-integration-table')
         return res.status(200).send(items);
     } catch (err) {
         logger.error(err);
