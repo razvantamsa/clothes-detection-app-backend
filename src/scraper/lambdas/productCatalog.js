e3054106b821c985eaaf87eea9754b2c909deea6
Razvan Tamsa <razvantamsa295@gmail.com>
2023-08-19 13:47:44 +0300

    scraper lambdas remane & deploy

---
+++ src/scraper/lambdas/productCatalog.js
@@ -0,0 +1,37 @@
+const { invokeAsyncFunction } = require('../../utils/aws/lambda');
+const utils = require('../../utils/scraping/cheerio/catalog.utils');
+const { loadHtml } = require('../../utils/scraping/cheerio/init');
+
+const { APP_MAX_PRODUCT_LIMIT } = process.env;
+
+exports.handler = async (event) => {
+    // console.log('Event payload: ', event);
+
+    // const { type, brand, baseUrl } = event, products = [];
+    // let url = baseUrl;
+
+    // try {
+    //     while(products.length <= APP_MAX_PRODUCT_LIMIT && url) {
+    //         const $ = await loadHtml(url);
+    //         const foundProducts = utils.scrapeProductsFromPage($);
+    //         url = utils.scrapeNextPageHref($);
+    //         products.push(...foundProducts);
+    //         console.log(products.length, url);
+    //     }
+    // } catch (error) {
+    //     throw error.message;
+    // }
+
+    // console.log(products.length);
+
+    // await invokeAsyncFunction(
+    //     'clothes-detection-scraper-dev-scrapeProductDetail',
+    //     { type, hrefs: products, brand },
+    // );
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    for (const record of event.Records) {
+      console.log('Processing message:', record.body);
+      // Add your processing logic here
+    }
+    return { statusCode: 200 };
+};
---
+++ src/scraper/requests/start.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { invokeAsyncFunction } = require('../../utils/aws/lambda');
+const { authorizerMiddleware } = require('../../utils/authorizer/authorizer');
+const { verifyTypeHeader } = require('../../utils/middelware/verifyTypeHeader');
+const { getItem } = require('../../utils/aws/dynamodb');
+const { sendEmail } = require('../../utils/aws/ses');
+const { sendMessageToQueue } = require('../../utils/aws/sqs');
+const router = express.Router();
+
+const { APP_DYNAMODB_INTEGRATIONS_TABLE } = process.env;
+
+router.post('/start', [authorizerMiddleware, verifyTypeHeader], async (req, res) => {
+    try {
+        const { brand, website, type} = req.headers;
+        const integration = await getItem(APP_DYNAMODB_INTEGRATIONS_TABLE, { brand, website });
+        
+        if(!integration) {
+            return res.status(404).send(`Integration for ${brand} on ${website} does not exist`);
+        }
+        if(!Object.prototype.hasOwnProperty.call(integration.types, type)) {
+            return res.status(400).send(`Integration for ${brand} on ${website} does not accept ${type}`);
+        }
+
+        // await invokeAsyncFunction(`clothes-detection-scraper-dev-scrapeProductCatalog`, { type, brand, baseUrl });
+
+        const Subject = 'Web Scraping API';
+        const Body = `Scraping ${website} for ${brand} ${type}, started on ${(new Date(Date.now())).toUTCString()}`;
+        // await sendEmail(Subject, Body);
+
+        await sendMessageToQueue(
+            JSON.stringify({Subject, Body}), 
+            'https://sqs.us-west-2.amazonaws.com/367859350018/catalog-queue.fifo',
+            'base-group-id'
+        );
+
+        return res.status(200).send(`Started scraping for ${brand}'s ${type}`);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err.message);
+    }
+});
+  
+module.exports = router;
