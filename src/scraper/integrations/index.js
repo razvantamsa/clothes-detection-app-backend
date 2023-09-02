1b23204fdb51aa18436fc5b1fb2e1b4ad8d465b0
Razvan Tamsa <razvantamsa295@gmail.com>
2023-09-02 21:53:20 +0300

    asos integration completed

--- src/scraper/integrations/index.js
+++ src/scraper/integrations/index.js
@@ -1,9 +1,12 @@
 const IntegrationUtils = {
+    // dummy integration
     ping: require('./ping'),
+
     aboutyou: require('./aboutyou'),
     emag: require('./emag'),
     fashionup: require('./fashionup'),
     bonprix: require('./bonprix'),
+    asos: require('./asos'),
 }
 
 module.exports = IntegrationUtils;
