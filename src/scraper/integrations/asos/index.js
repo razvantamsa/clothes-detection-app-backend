605e6ddf5d8e1050129c5b6bf3fa93029f3c1200
Razvan Tamsa <razvantamsa295@gmail.com>
2023-09-02 21:52:42 +0300

    scrape catalog completed

--- src/scraper/integrations/asos/index.js
+++ src/scraper/integrations/asos/index.js
@@ -2,8 +2,40 @@ const { loadHtml } = require("../../../utils/scraping/cheerio/init");
 const { loadDynamicPage } = require("../../../utils/scraping/puppeteer/init");
 const CheerioUtils = require("./cheerio");
 const PuppeteerUtils = require("./puppeteer");
+const { APP_MAX_PRODUCT_LIMIT, MAX_ATTEMPTS } = process.env;
 
 const Utils = {
+    scrapeProductCatalog: async (integration, type, sendToWorker) => {
+        let url = integration.types[type];
+        
+        let products = [], attempts = 0;
+        try {
+            while(products.length <= APP_MAX_PRODUCT_LIMIT && attempts <= MAX_ATTEMPTS) {
+                console.log(products.length);
+
+                const $ = await loadHtml(url);
+                if(!$) { break; }
+
+                const foundProducts = CheerioUtils.scrapeProductsFromPage($);
+                products.push(...foundProducts);
+                products = [ ...new Set(products)];
+                url = CheerioUtils.nextPageUrl(url);
+                console.log(url);
+                attempts+=1;                
+            }
+        } catch (error) {
+            throw error.message;
+        }
+
+        console.log(products, products.length);
+    
+        for (const product of products) {
+            await sendToWorker({ integration, type, href: product });
+        }
+
+        return products;
+    },
+
     scrapeProductDetail: async (integration, type, href) => {
         const [page, closeBrowserCallback] = await loadDynamicPage();
         const $ = await loadHtml(href);
